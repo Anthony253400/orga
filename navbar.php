0738e4f3331b6af23a6577fa370408666ef8e998
Anthony253400 <mirandanthony2005@yahoo.fr>
2025-10-23 14:02:18 +0000

    image

--- navbar.php
+++ navbar.php
@@ -2,3 +2,20 @@
 $base = '/orgaa_tp'; 
 ?>
 
+
+<nav class="navbar">
+  <div class="nav-gauche">
+    <img src="images/logo.png" alt="logo">
+  </div>
+  
+  <div class="nav-center">
+    <a href="index.php">
+      <span>Orga +</span>
+    </a>
+  </div>
+    
+  <div class="nav-droite">
+
+  </div>
+</nav>
+
